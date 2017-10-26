f5817cd24f6cdc3449cfb861cc66427c603a523c
Tom Berry <tberry860@gmail.com>
2017-10-26 12:17:05 -0700

    fix

--- ios/RNTVideoView.h
+++ ios/RNTVideoView.h
@@ -10,4 +10,13 @@
 @property (nonatomic, weak) id<RNTVideoViewDelegate> delegate;
 @property (nonatomic, weak, readonly) UIView *videoContainerView;
 
+@property (nonatomic, copy) NSString *host;
+@property (nonatomic, copy) NSString *token;
+@property (nonatomic, copy) NSString *displayName;
+@property (nonatomic, copy) NSString *resourceId;
+
+@property (nonatomic, copy) RCTBubblingEventBlock onConnect;
+@property (nonatomic, copy) RCTBubblingEventBlock onDisconnect;
+@property (nonatomic, copy) RCTBubblingEventBlock onFailure;
+
 @end
